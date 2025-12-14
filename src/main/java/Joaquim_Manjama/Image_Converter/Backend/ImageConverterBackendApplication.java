64a5e5bd76292bcf08c1287c140c0f79454015da
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-12-14 05:20:53 +0000

    first commit

---
+++ src/main/java/Joaquim_Manjama/Image_Converter/Backend/ImageConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package Joaquim_Manjama.Image_Converter.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageConverterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageConverterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Joaquim_Manjama/Image_Converter/Backend/ImageConverterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Joaquim_Manjama.Image_Converter.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageConverterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
