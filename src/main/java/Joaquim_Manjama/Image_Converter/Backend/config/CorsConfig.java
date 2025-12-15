f4f674430d2b42e76202d81a277c3addc25a00a8
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-12-15 08:25:16 +0000

    getting ready for first deploy

--- src/main/java/Joaquim_Manjama/Image_Converter/Backend/config/CorsConfig.java
+++ src/main/java/Joaquim_Manjama/Image_Converter/Backend/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package Joaquim_Manjama.Image_Converter.Backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,17 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    private String FRONTEND_URL;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // allow all paths
-                        .allowedOrigins("*") // your React origin
-                        .allowedMethods("*")
+                registry.addMapping("/**")
+                        .allowedOrigins(FRONTEND_URL)
+                        .allowedMethods("GET", "POST")
                         .allowedHeaders("*");
             }
         };
     }
 }
-
